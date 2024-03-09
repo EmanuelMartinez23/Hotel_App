9b6e5c8d78ace4c450c1f44021a611635826af4c
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-03-08 18:05:27 -0600

    Se desarrollarón las 3 funciones de ClienteDAOImpl.java para la tabla clientes, la cual era borrar, eliminar, listar clientes de la base de datos hotelDB

--- src/main/java/org/devemanuel/Main.java
+++ src/main/java/org/devemanuel/Main.java
@@ -1,13 +1,32 @@
 package org.devemanuel;
 
 
+import org.devemanuel.dao.ClienteDAOImpl;
+import org.devemanuel.model.Cliente;
 import org.devemanuel.util.DataBaseUtil;
 import org.devemanuel.util.DataSource;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DataBaseUtil.getconnection());
-        System.out.println(DataSource.getConnection());
 
+        ClienteDAOImpl clienteDAO = new ClienteDAOImpl();
+//        List<Cliente> clientes = clienteDAO.listarCliente();
+//        for (Cliente c : clientes) {
+//            System.out.println(c.getCliente_id());
+//            System.out.println(c.getNombre());
+//            System.out.println(c.getEmail());
+//            System.out.println(c.getTelefono());
+//        }
+//
+//        boolean b = clienteDAO.eliminarCliente(4);
+//        if (b){
+//            System.out.println("Se eleminimo el cliente correctamente");
+//
+//        }
+
+        boolean obama = clienteDAO.agregarCliente(new Cliente("Obama", "usa.gov.us.com", "32456780", 4));
+        System.out.println(obama);
     }
 }
--- src/main/java/org/devemanuel/dao/ClienteDAOImpl.java
+++ src/main/java/org/devemanuel/dao/ClienteDAOImpl.java
@@ -1,7 +1,10 @@
 package org.devemanuel.dao;
 
 import org.devemanuel.model.Cliente;
+import org.devemanuel.util.DataBaseUtil;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 /*
 Clase que va a tener la logica para los metodos de los clientes
@@ -9,16 +12,90 @@ Clase que va a tener la logica para los metodos de los clientes
 public class ClienteDAOImpl implements ClienteDAO{
     @Override
     public List<Cliente> listarCliente() {
-        return null;
+        // Creamos una lista donde vamos a almacenar los clientes
+        List<Cliente> listaClientes = new ArrayList<>();
+
+        // conseguimos una conexion hacia la base de datos
+        try (Connection connection = DataBaseUtil.getconnection()) {
+            // consulta SQL
+            String sql = "SELECT * FROM cliente";
+            // creamos una declaración con un tipo de resulset con scroll y que no se pueda modificar solo leer
+            try (
+                    Statement sts = connection.createStatement(
+                            ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY)
+            ) {
+                // ejecutamos la query y obtenemos los resultados
+                try (ResultSet resultSet = sts.executeQuery(sql)) {
+                    // iteramos sobre el resultSet
+                    while (resultSet.next()){
+                        // obtenemos el nombre
+                        String nombre =  resultSet.getString("nombre");
+                        String email =  resultSet.getString("email");
+                        String telefono =  resultSet.getString("telefono");
+                        int cliente_id =  resultSet.getInt("cliente_id");
+                        // vamos a agregar a la lista
+                        listaClientes.add(new Cliente(nombre,email,telefono,cliente_id));
+                    }
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listaClientes;
     }
 
     @Override
     public boolean eliminarCliente(int id) {
-        return false;
+        // variable que almacenara el resultado de la operación
+        int f = 0;
+
+        // conseguimos una conexion hacia la base de datos
+        try (Connection connection = DataBaseUtil.getconnection()) {
+            // consulta SQL para borrar un cliente por su ID
+            String sql = "DELETE FROM cliente WHERE cliente_id = ?";
+            // creamos una declaración prepqrada con un tipo de resulset con scroll y que no se pueda modificar solo leer
+            try (
+                    PreparedStatement ps = connection.prepareStatement(
+                            sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY) ){
+                // pasamos el parametro de la declaración preparada
+                ps.setInt(1,id);
+                // ejecutamos la query y obtenemos si se borro correctamente
+                f = ps.executeUpdate();
+                // retorna falso si es diferente a 1, que 1 regresaria executeUpdate() si borro
+                return f == 1;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public boolean agregarCliente(Cliente h) {
-        return false;
+    public boolean agregarCliente(Cliente cliente) {
+        // variable que almacenara el resultado de la operación
+        int f = 0;
+        // conseguimos una conexion hacia la base de datos
+        try (Connection connection = DataBaseUtil.getconnection()) {
+            // consulta SQL para agregar  un cliente
+            String sql = "INSERT INTO cliente(cliente_id, nombre, email, telefono) VALUES (?,?,?,?)";
+            // creamos una declaración prepqrada con un tipo de resulset con scroll y que no se pueda modificar solo leer
+            try (
+                    PreparedStatement ps = connection.prepareStatement(
+                            sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY) ){
+                // pasamos el parametro de la declaración preparada
+                ps.setInt(1,cliente.getCliente_id());
+                ps.setString(2,cliente.getNombre());
+                ps.setString(3,cliente.getEmail());
+                ps.setString(4,cliente.getTelefono());
+
+                // ejecutamos la query y obtenemos si se agrego correctamente
+                f = ps.executeUpdate();
+                // retorna falso si es diferente a 1, que 1 regresaria executeUpdate() si agrego correctamente
+                return f == 1;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
